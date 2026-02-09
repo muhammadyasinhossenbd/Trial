c278c11d0ea0be95ed20ab539843a5f67dc13652
muhammadyasinhossenbd <muhammadyasinhossenbd@gmail.com>
2026-02-09 11:40:55 +0600

    added

---
+++ number.js
@@ -0,0 +1,10 @@
+var orangeprice = 20;
+var chocolate = 0.5;
+// var appleprice = parseInt("22.8");
+var appleprice = parseFloat("22.8");
+
+console.log(orangeprice);
+console.log(appleprice);
+console.log(orangeprice+appleprice);
+
+
