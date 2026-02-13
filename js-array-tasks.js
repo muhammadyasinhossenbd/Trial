921723e7104ce06a30ada7e4a82757a8b441dc4b
muhammadyasinhossenbd <muhammadyasinhossenbd@gmail.com>
2026-02-13 12:43:14 +0600

    Task

---
+++ js-array-tasks.js
@@ -0,0 +1,29 @@
+//Task 01
+
+// const fruits = ['apple', 'orange', 'Grape', 'Banana', 'Watermelon' ] 
+// console.log(fruits[3]) // console log the 3rd element
+// fruits[2] = 'jambura'; //Changing the value of array inside using index
+// console.log(fruits)
+
+
+//Task 02
+// const destinations = ['Sylhet', 'Saint Martin', 'Bandarban']
+// console.log(destinations)
+// destinations.push('Cox bazar');
+// console.log(destinations)
+// destinations.push('Rajshahi', 'Sri Mangal');
+// console.log(destinations)
+// destinations.pop()                  //Remove Last element of array
+// console.log(destinations)
+// destinations.shift();               //Remove First element of array
+// console.log(destinations)
+// destinations.unshift("Birisiri");   //Add First element to array
+// console.log(destinations)
+
+
+//Task 03
+const books = ['Math', 'English', 'Bangla', 'Science', 'ICT', 'IE']
+console.log(books)
+console.log(books.includes('Math'))
+books.push('Biology')
+console.log(books)
